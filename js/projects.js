0d6508af227c734d415fdd9654433349a189bc56
James Brennan <james@jamesbrennan.ca>
2016-03-10 21:29:01 -0800

    Projects page

---
+++ js/projects.js
@@ -0,0 +1,135 @@
+$(function(){
+  $("#projects").elastic_grid({
+    // moveup, scaleup, fallperspective, fly, flip, helix , popup
+    'filterEffect': 'popup',
+    'hoverDirection': true,
+    'hoverDelay': 0,
+    'hoverInverse': false,
+    'expandingSpeed': 500,
+    'expandingHeight': 500,
+    'items':
+    [
+      {
+        'title': 'Gallery 1',
+        'description': 'Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage soko coriander sweet pepper water spinach winter purslane shallot tigernut lentil beetroot.Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage.',
+        'thumbnail': ['images/placeholder.jpg', 'images/placeholder.jpg', 'images/placeholder.jpg', 'images/placeholder.jpg', 'images/placeholder.jpg'],
+        'large': ['images/placeholder-large.jpg', 'images/placeholder-large.jpg', 'images/placeholder-large.jpg', 'images/placeholder-large.jpg', 'images/placeholder-large.jpg'],
+        'button_list': [],
+        'tags': ['Portrait']
+      },
+      {
+        'title': 'Swiss chard pumpkin',
+        'description': 'Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage soko coriander sweet pepper water spinach winter purslane shallot tigernut lentil beetroot.Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage.',
+        'thumbnail': ['images/placeholder.jpg', 'images/placeholder.jpg', 'images/placeholder.jpg', 'images/placeholder.jpg'],
+        'large': ['images/placeholder-large.jpg', 'images/placeholder-large.jpg', 'images/placeholder-large.jpg', 'images/placeholder-large.jpg'],
+        'button_list': [],
+        'tags': ['Landscape']
+      },
+      {
+        'title': 'Spinach winter purslane',
+        'description': 'Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage soko coriander sweet pepper water spinach winter purslane shallot tigernut lentil beetroot.Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage.',
+        'thumbnail': ['images/placeholder.jpg','images/placeholder.jpg', 'images/placeholder.jpg', 'images/placeholder.jpg'],
+        'large': ['images/placeholder-large.jpg','images/placeholder-large.jpg', 'images/placeholder-large.jpg', 'images/placeholder-large.jpg'],
+        'button_list': [],
+        'tags': ['Portrait', 'Landscape']
+      },
+      {
+        'title': 'Aubergine napa cabbage',
+        'description': 'Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage soko coriander sweet pepper water spinach winter purslane shallot tigernut lentil beetroot.Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage.',
+        'thumbnail': ['images/placeholder.jpg', 'images/placeholder.jpg', 'images/placeholder.jpg', 'images/placeholder.jpg', 'images/placeholder.jpg'],
+        'large': ['images/placeholder-large.jpg', 'images/placeholder-large.jpg', 'images/placeholder-large.jpg', 'images/placeholder-large.jpg', 'images/placeholder-large.jpg'],
+        'button_list': [],
+        'tags': ['Portrait']
+      },
+      {
+        'title': 'Swiss chard pumpkin',
+        'description': 'Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage soko coriander sweet pepper water spinach winter purslane shallot tigernut lentil beetroot.Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage.',
+        'thumbnail': ['images/placeholder.jpg', 'images/placeholder.jpg', 'images/placeholder.jpg', 'images/placeholder.jpg'],
+        'large': ['images/placeholder-large.jpg', 'images/placeholder-large.jpg', 'images/placeholder-large.jpg', 'images/placeholder-large.jpg'],
+        'button_list': [],
+        'tags': ['Landscape']
+      },
+      {
+        'title': 'Spinach winter purslane',
+        'description': 'Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage soko coriander sweet pepper water spinach winter purslane shallot tigernut lentil beetroot.Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage.',
+        'thumbnail': ['images/placeholder.jpg','images/placeholder.jpg', 'images/placeholder.jpg', 'images/placeholder.jpg'],
+        'large': ['images/placeholder-large.jpg','images/placeholder-large.jpg', 'images/placeholder-large.jpg', 'images/placeholder-large.jpg'],
+        'button_list': [],
+        'tags': ['Vintage']
+      },
+      {
+        'title': 'Spinach winter purslane',
+        'description': 'Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage soko coriander sweet pepper water spinach winter purslane shallot tigernut lentil beetroot.Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage.',
+        'thumbnail': ['images/placeholder.jpg','images/placeholder.jpg', 'images/placeholder.jpg', 'images/placeholder.jpg'],
+        'large': ['images/placeholder-large.jpg','images/placeholder-large.jpg', 'images/placeholder-large.jpg', 'images/placeholder-large.jpg'],
+        'button_list': [],
+        'tags': ['Portrait']
+      },
+      {
+        'title': 'Azuki bean',
+        'description': 'Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage soko coriander sweet pepper water spinach winter purslane shallot tigernut lentil beetroot.Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage.',
+        'thumbnail': ['images/placeholder.jpg', 'images/placeholder.jpg', 'images/placeholder.jpg', 'images/placeholder.jpg', 'images/placeholder.jpg'],
+        'large': ['images/placeholder-large.jpg', 'images/placeholder-large.jpg', 'images/placeholder-large.jpg', 'images/placeholder-large.jpg', 'images/placeholder-large.jpg'],
+        'button_list': [],
+        'tags': ['Vintage']
+      },
+      {
+        'title': 'Swiss chard pumpkin',
+        'description': 'Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage soko coriander sweet pepper water spinach winter purslane shallot tigernut lentil beetroot.Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage.',
+        'thumbnail': ['images/placeholder.jpg', 'images/placeholder.jpg', 'images/placeholder.jpg', 'images/placeholder.jpg'],
+        'large': ['images/placeholder-large.jpg', 'images/placeholder-large.jpg', 'images/placeholder-large.jpg', 'images/placeholder-large.jpg'],
+        'button_list': [],
+        'tags': ['Landscape']
+      },
+      {
+        'title': 'Winter purslane',
+        'description': 'Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage soko coriander sweet pepper water spinach winter purslane shallot tigernut lentil beetroot.Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage.',
+        'thumbnail': ['images/placeholder.jpg','images/placeholder.jpg', 'images/placeholder.jpg', 'images/placeholder.jpg'],
+        'large': ['images/placeholder-large.jpg','images/placeholder-large.jpg', 'images/placeholder-large.jpg', 'images/placeholder-large.jpg'],
+        'button_list': [],
+        'tags': ['Portrait']
+      },
+      {
+        'title': 'Spinach winter purslane',
+        'description': 'Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage soko coriander sweet pepper water spinach winter purslane shallot tigernut lentil beetroot.Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage.',
+        'thumbnail': ['images/placeholder.jpg','images/placeholder.jpg', 'images/placeholder.jpg', 'images/placeholder.jpg'],
+        'large': ['images/placeholder-large.jpg','images/placeholder-large.jpg', 'images/placeholder-large.jpg', 'images/placeholder-large.jpg'],
+        'button_list': [],
+        'tags': ['Vintage']
+      },
+      {
+        'title': 'Spinach winter purslane',
+        'description': 'Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage soko coriander sweet pepper water spinach winter purslane shallot tigernut lentil beetroot.Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage.',
+        'thumbnail': ['images/placeholder.jpg','images/placeholder.jpg', 'images/placeholder.jpg', 'images/placeholder.jpg'],
+        'large': ['images/placeholder-large.jpg','images/placeholder-large.jpg', 'images/placeholder-large.jpg', 'images/placeholder-large.jpg'],
+        'button_list': [],
+        'tags': ['Portrait', 'Landscape']
+      },
+      {
+        'title': 'Azuki bean',
+        'description': 'Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage soko coriander sweet pepper water spinach winter purslane shallot tigernut lentil beetroot.Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage.',
+        'thumbnail': ['images/placeholder.jpg', 'images/placeholder.jpg', 'images/placeholder.jpg', 'images/placeholder.jpg', 'images/placeholder.jpg'],
+        'large': ['images/placeholder-large.jpg', 'images/placeholder-large.jpg', 'images/placeholder-large.jpg', 'images/placeholder-large.jpg', 'images/placeholder-large.jpg'],
+        'button_list': [],
+        'tags': ['Vintage']
+      },
+      {
+        'title': 'Swiss chard pumpkin',
+        'description': 'Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage soko coriander sweet pepper water spinach winter purslane shallot tigernut lentil beetroot.Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage.',
+        'thumbnail': ['images/placeholder.jpg', 'images/placeholder.jpg', 'images/placeholder.jpg', 'images/placeholder.jpg'],
+        'large': ['images/placeholder-large.jpg', 'images/placeholder-large.jpg', 'images/placeholder-large.jpg', 'images/placeholder-large.jpg'],
+        'button_list': [],
+        'tags': ['Landscape']
+      },
+      {
+        'title': 'Spinach winter purslane',
+        'description': 'Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage soko coriander sweet pepper water spinach winter purslane shallot tigernut lentil beetroot.Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage.',
+        'thumbnail': ['images/placeholder.jpg','images/placeholder.jpg', 'images/placeholder.jpg', 'images/placeholder.jpg'],
+        'large': ['images/placeholder-large.jpg','images/placeholder-large.jpg', 'images/placeholder-large.jpg', 'images/placeholder-large.jpg'],
+        'button_list': [],
+        'tags': ['Vintage', 'Landscape']
+      }
+
+    ]
+  });
+});
