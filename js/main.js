014fe325fa007ba3d6f1edc2ac6ef5f41d4641ab
James Brennan <james@jamesbrennan.ca>
2016-03-23 20:20:26 -0700

    Transition projects back in

--- js/main.js
+++ js/main.js
@@ -1,28 +1,6 @@
 var $ = $ || {};
 
 $(document).ready(function() {
-  console.debug("Load");
-
-  var showLetterTimeout = null;
-
-  function showLetter($container, word) {
-    var letters = word.split(""),
-        letter = letters.shift();
-    if(showLetterTimeout) clearTimeout(showLetterTimeout);
-
-    $("<span>").hide()
-      .text(letter)
-      .appendTo($container)
-      .fadeIn(120);
-
-    if(letters.length > 0) {
-      showLetterTimeout = setTimeout(function() {
-        showLetter($container, letters.join(""));
-      }, 60);
-    }
-
-    return null;
-  }
 
   function moveOffscreen($el, windowWidth, windowHeight) {
     var top, left, position = $el.position();
@@ -37,39 +15,37 @@ $(document).ready(function() {
       left = "-500px";
     }
     // TODO hide once offscreen
-    $el.animate({ top: top, left: left});
+    $el.animate({ top: top, left: left}, 400, "swing", function() {
+      $el.hide();
+    });
+    $el.data('original-position', position);
+  }
+
+  function moveOnscreen($el, windowWidth, windowHeight) {
+    $el.show().animate($el.data('original-position'));
   }
 
   $(".projects li").on("click", function(e) {
     var $this = $(this),
         top = $this.position().top + 85,
-        left = $this.position().left + 140;
-    // $(".active").removeClass("active");
-    // $(".projects").addClass("active");
-    // $this.addClass("active");
-    $project = $(".project-container");
+        left = $this.position().left + 140,
+        $project = $(".project-container");
 
     var $window = $(window),
         windowWidth = $window.width(),
         windowHeight = $window.height();
+
     $(".projects li").each(function() {
-      console.log(this);
       moveOffscreen($(this), windowWidth, windowHeight);
     });
-    $($this.data("target")).fadeIn(700);
 
-    // $project
-    //   .css("top", top + "px")
-    //   .css("left", left + "px")
-    //   .text("")
-    //   .show();
-    // showLetter($project, $this.data("name"));
+    $($this.data("target")).fadeIn(700);
   });
 
   $(".project-details-close").on("click", function(e) {
-    $(".projects li")
-      .css("top", "")
-      .css("left", "");
+    $(".projects li").each(function() {
+      moveOnscreen($(this));
+    });
     $(".project-details").fadeOut(500);
   });
 
