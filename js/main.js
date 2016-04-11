d36286ca908cc55a8067de28039f94661be7dd57
James Brennan <james@jamesbrennan.ca>
2016-04-10 22:38:18 -0700

    Cleanup

--- js/main.js
+++ js/main.js
@@ -2,36 +2,14 @@ var $ = $ || {};
 
 $(document).ready(function() {
 
-  function moveOffscreen($el, windowWidth, windowHeight) {
-    var top, left, position = $el.position();
-    if((position.top + 200) > (windowHeight / 2)) {
-      top = (windowHeight + 500) + "px";
-    } else {
-      top = "-500px";
-    }
-    if((position.left + 200) > (windowWidth / 2)) {
-      left = (windowWidth + 500) + "px";
-    } else {
-      left = "-500px";
-    }
-    // TODO hide once offscreen
-    $el.animate({ top: top, left: left}, 400, "swing", function() {
-      $el.hide();
-    });
-    $el.data('original-position', position);
-  }
-
   function showProject(target) {
     var $project = $(".project-container"),
         $window = $(window),
         windowWidth = $window.width(),
         windowHeight = $window.height();
 
-    $(".projects li").each(function() {
-      moveOffscreen($(this), windowWidth, windowHeight);
-    });
-
-    $(target).fadeIn(700);
+    $(".projects li").fadeOut(500);
+    $(target).fadeIn(500);
   }
 
   function resetProjects() {
@@ -48,15 +26,7 @@ $(document).ready(function() {
     $("html, body").animate({ scrollTop: 0 }, scrollTime, "swing", function() {
       $(".project-details").fadeOut(500);
 
-      $(".projects li").each(function () {
-        var $this = $(this),
-            originalPosition = $this.data('original-position');
-        if(originalPosition) {
-          $this.show().animate(originalPosition, function() {
-            $this.css({top: "", left: ""});
-          });
-        }
-      });
+      $(".projects li").fadeIn(500);
     });
   }
 
@@ -107,7 +77,17 @@ $(document).ready(function() {
     }
   }
 
+  function fixX() {
+    var $body = $("body");
+    if ($body[0].scrollTop > 215) {
+      $body.addClass("fixed");
+    } else {
+      $body.removeClass("fixed");
+    }
+  }
+
   navigateTo(window.location.hash);
+  fixX();
 
   $(document).on("keyup", escProject);
 
@@ -118,4 +98,6 @@ $(document).ready(function() {
     } catch (e) {}
     navigateTo(window.location.hash, oldHash);
   });
+
+  $(document).on("scroll", fixX);
 });
